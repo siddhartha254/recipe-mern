0e31a855d0d7614b9393788facd04f9d8d26925c
Siddhartha <siddhartha.arora4@gmail.com>
2023-06-10 18:29:13 +0530

    configured auth backend

---
+++ server/src/routes/users.js
@@ -0,0 +1,45 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/Users.js";
+
+const router = express.Router();
+
+
+router.post("/register", async (req,res) =>{
+    const {username, password} = req.body;
+    const user = await UserModel.findOne({username});
+
+    if(user){
+        return res.json({message: "User already exists"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10);
+    const newUser = new UserModel({username,password:hashedPassword});
+    await newUser.save();
+
+
+    res.json({message: "Registration Successful"});
+});
+
+
+router.post("/login", async (req,res) =>{
+    const {username, password} = req.body;
+    const user = await UserModel.findOne({username});
+
+    if(!user){
+        return res.json({messsage: "No account with this username"});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid){
+        return res.json({message: "Wrong Username or Password"});
+    }
+
+    const token = jwt.sign({id: user._id}, "secret");
+    res.json({token, userID:user._id});
+
+});
+
+export {router as userRouter};
