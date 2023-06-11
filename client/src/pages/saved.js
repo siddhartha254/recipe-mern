75078bdd106600dd8909ad297375e58079b3ea63
Siddhartha <siddhartha.arora4@gmail.com>
2023-06-12 00:44:28 +0530

    fixed bugs

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import {useGetUserID} from "../hooks/useGetUserID";
 
 const Saved = () =>{
+
+    const [savedRecipes, setSavedRecipes] = useState([]);
+    const userID = useGetUserID();
+    
+    useEffect(()=> {
+
+        const fetchSavedRecipe = async () =>{
+            try{
+                const response = await axios.get(`http://localhost:3001/recipes/savedRecipes/${userID}`);
+                setSavedRecipes(response.data.savedRecipes);
+                //console.log(response.data);
+            }catch(err){
+                console.error(err);
+            }
+        }
+        fetchSavedRecipe();
+    }, []);
+
+
     return(
         <div>
-            Saved
+            <h1>Saved Recipes</h1>
+
+            <ul>
+                {savedRecipes.map((recipe)=>(
+                    <li key={recipe._id}>
+                        <div>
+                            <h2>{recipe.name}</h2>
+                        </div>
+                        <div className="instructions">
+                            <p>{recipe.instructions}</p>
+                        </div>
+                        <img src={recipe.imgUrl} alt={recipe.name}/>
+                        <p>Cooking Time: {recipe.cookingTime} minutes</p>
+                    </li>
+                ))}
+            </ul>
+
         </div>
     );
 }
