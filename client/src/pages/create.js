78110269ee90b7f37efd89bb633f6d5932060a1b
Siddhartha <siddhartha.arora4@gmail.com>
2023-06-11 17:52:44 +0530

    added backend for saved recipes

--- client/src/pages/create.js
+++ client/src/pages/create.js
@@ -1,9 +1,80 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
 
 const Create = () =>{
+    
+    const userID = useGetUserID();
+
+    const [recipe, setRecipe] = useState({
+        name: "",
+        ingredients:[],
+        instructions: "",
+        imageUrl: "",
+        cookingTime: 0,
+        userOwner: userID,
+    });
+
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setRecipe({...recipe, [name]:value});
+    };
+
+    const handleIngredientChange = (event, idx) => {
+        const {value} = event.target;
+        const ingredients = recipe.ingredients;
+        ingredients[idx] = value;
+        setRecipe({...recipe, ingredients});
+    };
+
+    const addIngredient = () =>{
+        setRecipe({...recipe, ingredients: [...recipe.ingredients, ""] });
+    };
+
+    //console.log(recipe);
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            await axios.post("http://localhost:3001/recipes", recipe);
+            alert("Your recipe has been added");
+        }catch(err){
+            console.error(err);
+        }
+    }
+
     return(
-        <div>
-            Create
+        <div className="create-recipe">
+            <h2>Add Recipe</h2>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name"name="name" onChange={handleChange}/>
+                
+                <label htmlFor="ingredients">Ingredients</label>
+
+                {recipe.ingredients.map((ingredient, idx) => (
+                    <input 
+                        key={idx} 
+                        type="text" 
+                        name="ingredients" 
+                        value={ingredient} 
+                        onChange={(event)=>handleIngredientChange(event,idx)}
+                    />
+                ))}
+                <button onClick={addIngredient} type="button">Add Ingredient</button>
+
+                <label htmlFor="instructions">Instructions</label>
+                <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
+                <label htmlFor="imageUrl">Image URL</label>
+                <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange} />
+                <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+                <input type="number" id="cookingTime" name="cookingTime"onChange={handleChange} />
+                
+                <button type="submit">Add</button>
+
+            </form>
+            
         </div>
     );
 }
